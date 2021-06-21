8f4d81c990aa9f5be34b4b12b53048f2832da5e3
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2021-06-21 14:48:41 +0700

    Update HCLSDKService.js

--- client/src/services/HCLSDKService.js
+++ client/src/services/HCLSDKService.js
@@ -1,9 +1,9 @@
+// Update the apiKey value below to a valid HealthcareLocator's apiKey
 const defaultSDKConfig = {
     apiKey: '30008a88a12364f2',
     // appName: 'https://github.com/nongnoochr',
     // appURL: 'https://github.com/nongnoochr'
 };
-
 
 export {
     defaultSDKConfig
