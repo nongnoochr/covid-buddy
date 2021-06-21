d3a7440b5d3615ae9421e81298f81193c7322686
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2021-06-21 14:55:34 +0700

    Update HCLSDKService.js

--- client/src/services/HCLSDKService.js
+++ client/src/services/HCLSDKService.js
@@ -1,9 +1,16 @@
 // Update the apiKey value below to a valid HealthcareLocator's apiKey
 const defaultSDKConfig = {
-    apiKey: '30008a88a12364f2',
-    // appName: 'https://github.com/nongnoochr',
-    // appURL: 'https://github.com/nongnoochr'
+    apiKey: '300131ea848b420a',
+    appName: 'COVID-19 Buddy',
+    appURL: 'https://covid-buddy.herokuapp.com/'
 };
+
+// --- OLD apiKey
+// const defaultSDKConfig = {
+//     apiKey: '30008a88a12364f2',
+//     // appName: 'https://github.com/nongnoochr',
+//     // appURL: 'https://github.com/nongnoochr'
+// };
 
 export {
     defaultSDKConfig
