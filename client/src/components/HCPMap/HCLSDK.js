fcbc201d4c06407f5b1ca49a266c01a9f3cfb234
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2021-06-27 14:11:14 +0700

    Update HCLSDK.js

--- client/src/components/HCPMap/HCLSDK.js
+++ client/src/components/HCPMap/HCLSDK.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { Badge, Form, Col } from 'react-bootstrap';
 
 import HCPContext from '../../store/hcp-context';
@@ -9,27 +9,29 @@ import classes from './HCLSDK.module.css';
 
 const HCLSDK = (props) => {
 
+    const hclSdkRef = useRef(null);
+
     // --- Contexts
     const ctx = useContext(HCPContext);
 
     // --- states
     const [appliedQuickSearch, setAppliedQuickSearch] = useState(ctx.quickSearch);
 
+    // --- Helpers
+    const isQuickSearchApplied = () => {
+        return (ctx.quickSearch && (ctx.quickSearch !== 'All'));
+    };
+
     // --- Effects
-    // Re initialize the HDLSDK to update view when the quicksearch value is updated
+
     useEffect(() => {
-
-        // eslint-disable-next-line no-undef
-        setAppliedQuickSearch(ctx.quickSearch);
-
-        const HCLSDK = document.querySelector('hcl-sdk');
 
         let curSDKConfig = {
             ...defaultSDKConfig
         };
 
         // Use the 'searchNearMe' screen if the quickSearch is set 
-        if (ctx.quickSearch && (ctx.quickSearch !== 'All')) {
+        if (isQuickSearchApplied()) {
             const spCode = getSpecialtyCode(ctx.quickSearch);
 
             curSDKConfig = {
@@ -40,31 +42,40 @@ const HCLSDK = (props) => {
                 }
             };
 
-            HCLSDK.init(curSDKConfig);
         } else {
             // Use a default search screen by default (Including 'All)
-            // Note that this setting currently does not re-render the SDK to the
-            // Homescreen if the 'searchNearMe' screen is active
-            // https://healthcareappchallenge.devpost.com/forum_topics/35113-some-tips-for-frontend-developer-to-use-the-sdk
             curSDKConfig = {
                 ...curSDKConfig,
                 entry: {
-                    screenName: 'home',
+                    screenName: 'home'
                 }
-            };
-
-            const p = HCLSDK.init(curSDKConfig);
-            p.then(() => {
-                HCLSDK.backToHome();
-            });            
+            };          
 
         }
 
-    }, [ctx.quickSearch])
+        hclSdkRef.current.init(curSDKConfig);
+        
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    // customElements.whenDefined('hcl-sdk').then(function () {
-    //         debugger
-    // });    
+    // Re initialize the HDLSDK to update view when the quicksearch value is updated
+    useEffect(() => {
+
+        // eslint-disable-next-line no-undef
+        setAppliedQuickSearch(ctx.quickSearch);
+
+        // Use the 'searchNearMe' screen if the quickSearch is set 
+        if (isQuickSearchApplied()) {
+            const spCode = getSpecialtyCode(ctx.quickSearch);
+            hclSdkRef.current.searchNearMe({specialtyCode: spCode});
+
+        } else {
+            hclSdkRef.current.backToHome();
+
+        }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ctx.quickSearch])
 
     return (
         <div>
@@ -91,7 +102,7 @@ const HCLSDK = (props) => {
             {/* SDK */}
 
             <div className={`sdk-parent-element ${classes['sdk-main-container']}`}>
-                <hcl-sdk></hcl-sdk>
+                <hcl-sdk ref={hclSdkRef} />
             </div>
         </div>
 
